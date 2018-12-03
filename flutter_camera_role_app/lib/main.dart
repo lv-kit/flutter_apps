b3ec1ba988ea8a5106e421f09facd2475a1c6976
TakehiroTakahashi <taka.mc.0509@gmail.com>
2018-12-04 02:53:34 +0900

    multi image pickerの追加

--- flutter_camera_role_app/lib/main.dart
+++ flutter_camera_role_app/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/asset.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 void main() => runApp(MyApp());
 
