adc1d2ebeeea68f515381c7da9a5de401e88e99b
TakehiroTakahashi <taka.mc.0509@gmail.com>
2018-12-12 17:17:34 +0900

    can start camera app

--- camera_app_sample/lib/main.dart
+++ camera_app_sample/lib/main.dart
@@ -8,112 +8,249 @@ import 'package:simple_permissions/simple_permissions.dart';
 
 List<CameraDescription> cameras;
 
-void main() => runApp(MyApp());
+Future<Null> main() async {
+  try {
+    cameras = await availableCameras();
+    await requestPermission();
+  } on CameraException catch (e) {
+    print(e);
+  }
+
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  CameraController controller;
+  String imagePath;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      key: _scaffoldKey,
+      appBar: new AppBar(
+        title: const Text('Camera example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: new Column(
+        children: <Widget>[
+          new Expanded(
+            child: new Container(
+              child: new Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: new Center(
+                  child: _cameraPreviewWidget(),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          _captureIconWidget(),
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _cameraTogglesRowWidget(),
+                _thumbnailWidget(),
+              ],
             ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // camera preview widget
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Tap a camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return new AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: new CameraPreview(controller),
+      );
+    }
+  }
+
+  // thumbnail widget
+  Widget _thumbnailWidget() {
+    return new Expanded(
+      child: new Align(
+        alignment: Alignment.centerRight,
+        child: imagePath == null
+            ? null
+            : new SizedBox(
+          child: new Image.file(new File(imagePath)),
+          width: 64.0,
+          height: 64.0,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // camera icon widget
+  Widget _captureIconWidget() {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        new IconButton(
+          icon: const Icon(Icons.camera_alt),
+          color: Colors.blue,
+          onPressed: controller != null &&
+              controller.value.isInitialized
+              ? onTakePictureButtonPressed
+              : null,
+        ),
+      ],
+    );
+  }
+
+  // camera switch in or out
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    if (cameras.isEmpty) {
+      return const Text('No camera fount');
+    } else {
+      for (CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          new SizedBox(
+            width: 90.0,
+            child: new RadioListTile<CameraDescription>(
+              title: new Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              value: cameraDescription,
+              groupValue: controller?.description,
+              onChanged: onNewCameraSelected,
+            ),
+          ),
+        );
+      }
+    }
+
+    return new Row(children: toggles,);
+  }
+
+  // toggle
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = new CameraController(CameraDescription(), ResolutionPreset.high);
+
+    controller.addListener(() {
+      if (mounted) setState(() {
+        //
+      });
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      print(e);
+    }
+
+    if (mounted) {
+      setState(() {
+        //
+      });
+    }
+  }
+
+  // when called touch camera icon
+  void onTakePictureButtonPressed() {
+    takePicture().then((String filePath) {
+      if (mounted) {
+        setState(() {
+          imagePath = filePath;
+        });
+      }
+    });
+  }
+
+  // タイムスタンプを返す関数
+  String timestamp() => new DateTime.now().millisecondsSinceEpoch.toString();
+
+  // カメラで撮影した画像を保存する関数
+  Future<String> takePicture() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+
+    Directory dir;
+    if (Platform.isAndroid) {
+      dir = await getExternalStorageDirectory(); // 外部ストレージに保存
+    } else if (Platform.isIOS) {
+      dir = await getTemporaryDirectory(); // 一時ディレクトリに保存
+    } else {
+      return null;
+    }
+
+    final String dirPath = '${dir.path}/Pictures/flutter_test';
+    await Directory(dirPath).create(recursive: true);
+    String filePath = '$dirPath/${timestamp()}.jpg';
+
+    if (controller.value.isTakingPicture) {
+      return null;
+    }
+
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e){
+      print(e);
+      return null;
+    }
+
+    if (Platform.isIOS) {
+      String tmpPath = filePath;
+      var savedFile = File.fromUri(Uri.file(tmpPath));
+      filePath = await ImagePickerSaver.saveFile(fileData: savedFile.readAsBytesSync());
+    }
+
+    return filePath;
+
+  }
+}
+
+// return camera icon function
+IconData getCameraLensIcon(CameraLensDirection direction) {
+  switch (direction) {
+    case CameraLensDirection.back:
+      return Icons.camera_rear;
+    case CameraLensDirection.front:
+      return Icons.camera_front;
+    case CameraLensDirection.external:
+      return Icons.camera;
+  }
+  throw new ArgumentError('Unkown lens direction');
+}
+
+// do permission function
+requestPermission() async {
+  if (Platform.isAndroid && !await SimplePermissions.checkPermission(Permission.WriteExternalStorage)) {
+    SimplePermissions.requestPermission(Permission.WriteExternalStorage);
+  } else if (Platform.isIOS && !await SimplePermissions.checkPermission(Permission.PhotoLibrary)) {
+    SimplePermissions.requestPermission(Permission.PhotoLibrary);
   }
 }
